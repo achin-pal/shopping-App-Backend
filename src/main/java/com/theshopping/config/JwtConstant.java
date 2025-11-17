5699dd9f90daf7376b5c6be6e1ab773304bcd659
Achin <achin.pal7@gmail.com>
2025-11-17 16:37:45 -0500

    initial security setup

---
+++ src/main/java/com/theshopping/config/JwtConstant.java
@@ -0,0 +1,8 @@
+package com.theshopping.config;
+
+public class JwtConstant {
+
+    public static final String SECRET_KEY ="jfnjewfafebewbfjweqfbqjfebqfqffqfacw";
+    public static final String HEADER ="Authorization";
+
+}
---
+++ src/main/java/com/theshopping/config/JwtProvider.java
@@ -0,0 +1,4 @@
+package com.theshopping.config;
+
+public class JwtProvider {
+}
